e1c057e17e99056fa879ac32681c76c50acf3ca0
Twix_ter <kirik_143@bk.ru>
2024-03-30 02:43:24 +0300

    all

--- src/services/getList.js
+++
@@ -1,9 +0,0 @@
-const Device = require("./../classes/Devices")
-function loopList(){
-    setInterval(() => {
-        Device.list().then(res => {
-            Device.cashList = {list: res}
-        }) 
-    }, 500)
-}
-module.exports = loopList
